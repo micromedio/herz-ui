57dbca566bc1e51ca685284c473eb0d147220280
Eduardo Campos de Souza <eduardofidera@gmail.com>
2021-01-14 20:38:59 -0300

    Add different sections

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# herz-ui
+
+Herz UI (/hɛrʦ/ - Hérts) is a design system containing a styleguide(colors, grid, icons, typography) and library of UI components
--- styleguide.config.js
+++ styleguide.config.js
@@ -1,9 +1,22 @@
 const path = require("path")
+const { version } = require("./package.json")
 
 module.exports = {
-  components: "src/components/**/*.tsx",
+  version: `${version}`,
   styleguideComponents: {
     Wrapper: path.join(__dirname, "src/styleguide/ThemeWrapper"),
   },
   propsParser: require("react-docgen-typescript").parse,
+  sections: [
+    {
+      name: "introduction",
+      content: "./docs/index.md",
+    },
+    {
+      name: "components",
+      content: "./docs/components.md",
+      skipComponentsWithoutExample: true,
+      components: "src/components/**/*.tsx",
+    },
+  ],
 }
