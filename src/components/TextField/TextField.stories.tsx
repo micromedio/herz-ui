9960201e64cd3f848884a1ca669dfceac692b4a2
Giuseppe Battistella <giuseppe.battistella@datarisk.io>
2021-01-28 17:03:39 -0300

    Added storybook

---
+++ src/components/TextField/TextField.stories.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react"
+import TextField, { TextFieldProps } from "./TextField"
+import { Meta, Story } from "@storybook/react/types-6-0"
+
+export default {
+  title: "Design System/TextField",
+  component: TextField,
+} as Meta
+
+const Template: Story<TextFieldProps> = (props: TextFieldProps) => {
+  const [value, setValue] = useState(props.value)
+
+  return (
+    <TextField
+      {...props}
+      value={value}
+      onChange={(event) => {
+        if (props.onChange) props.onChange(event)
+        setValue(event.target.value)
+      }}
+    />
+  )
+}
+
+export const Example = Template.bind({})
+Example.args = {
+  id: "field-id",
+  label: "Label",
+  helperText: "Text to help explain the input",
+}
+
+export const WithoutLabels = Template.bind({})
+WithoutLabels.args = {}
+
+export const Filled = Template.bind({})
+Filled.args = {
+  ...Example.args,
+  value: "Filled input",
+}
