af2b8107f0681098b98e82e9f09a1c8be581ad38
Giuseppe Battistella <giusbattistella@gmail.com>
2021-02-02 13:00:45 -0300

    Refactor test assertion

--- src/components/TextField/TextField.test.tsx
+++ src/components/TextField/TextField.test.tsx
@@ -1,39 +1,43 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render } from "@testing-library/react"
 import { axe } from "jest-axe"
 import TextField from "./TextField"
 
 describe("TextField", () => {
   test("label is shown", async () => {
     // Arrange
-    render(<TextField label="TEST_LABEL" id="TEST_ID" />)
+    const { getByLabelText } = render(
+      <TextField label="TEST_LABEL" id="TEST_ID" />
+    )
 
     // Assert
-    expect(screen.getByLabelText("TEST_LABEL")).toBeTruthy()
+    expect(getByLabelText("TEST_LABEL")).toBeInTheDocument()
   })
 
   test("helper text is shown", async () => {
     // Arrange
-    render(<TextField helperText="TEST_HELPER_TEXT" />)
+    const { getByText } = render(<TextField helperText="TEST_HELPER_TEXT" />)
 
     // Assert
-    expect(screen.getByText("TEST_HELPER_TEXT")).toBeTruthy()
+    expect(getByText("TEST_HELPER_TEXT")).toBeInTheDocument()
   })
 
   test("unit is shown", async () => {
     // Arrange
-    render(<TextField unit="TEST_UNIT" />)
+    const { getByText } = render(<TextField unit="TEST_UNIT" />)
 
     // Assert
-    expect(screen.getByText("TEST_UNIT")).toBeTruthy()
+    expect(getByText("TEST_UNIT")).toBeInTheDocument()
   })
 
   test("input is disabled", async () => {
     // Arrange
-    render(<TextField label="INPUT_LABEL" id="TEST_ID" disabled />)
+    const { getByLabelText } = render(
+      <TextField label="INPUT_LABEL" id="TEST_ID" disabled />
+    )
 
     // Assert
-    expect(screen.getByLabelText("INPUT_LABEL")).toBeDisabled()
+    expect(getByLabelText("INPUT_LABEL")).toBeDisabled()
   })
 
   test("passes a11y check", async () => {
