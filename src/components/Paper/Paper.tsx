bedaedf33045695e5eed021c6ba6cff4c73b37d8
Giuseppe Battistella <giusbattistella@gmail.com>
2021-02-09 10:47:19 -0300

    DEV-18: Updated elevations

--- src/components/Paper/Paper.tsx
+++ src/components/Paper/Paper.tsx
@@ -2,19 +2,17 @@ import React, { useMemo } from "react"
 import { Box } from "theme-ui"
 
 export interface PaperProps {
-  /**
-   * Paper elevation
-   */
+  /** Paper elevation */
   elevation?: number
-  /**
-   * The content of the component
-   */
+  /** The content of the component */
   children?: React.ReactNode
 }
 
 const Paper = ({ elevation = 1, children }: PaperProps) => {
   const boxShadow = useMemo(() => {
-    if (elevation) return "0px 0px 8px rgba(0, 0, 0, 0.04)"
+    if (elevation === 1) return "0px 1px 12px rgba(0, 0, 0, 0.04);"
+    if (elevation >= 2) return "0px 1px 12px rgba(0, 0, 0, 0.16)"
+
     return "none"
   }, [elevation])
 
