009e07a0a74232523d31c3dd5b9d060d9d631a0c
Giuseppe Battistella <giusbattistella@gmail.com>
2021-02-11 16:51:30 -0300

    DEV-17: Improved pagination

--- src/components/Table/Table.stories.tsx
+++ src/components/Table/Table.stories.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useMemo, useState } from "react"
 import Table, { TableProps } from "./Table"
 import { Meta, Story } from "@storybook/react/types-6-0"
 
@@ -12,78 +12,136 @@ const Template: Story<TableProps> = (props) => <Table {...props} />
 // Each story then reuses that template
 export const Example = Template.bind({})
 
-Example.args = {
-  title: "Table Title",
-  columns: [
-    {
-      Header: "Study #",
-      accessor: "id",
-      highlight: true,
+const columns = [
+  {
+    Header: "Study #",
+    accessor: "id",
+    highlight: true,
+  },
+  {
+    Header: "Patient",
+    accessor: "patient.name",
+  },
+  {
+    Header: "Referring physician",
+    accessor: "physician.name",
+  },
+  {
+    Header: "Study status",
+    accessor: "status",
+    Cell: ({ value }: { value: "draft" | "pre-registered" | "pending" }) => {
+      return (
+        {
+          draft: "In Draft",
+          "pre-registered": "Pre-registered",
+          pending: "Pending report",
+        }[value] ?? ""
+      )
     },
-    {
-      Header: "Patient",
-      accessor: "patient.name",
-      // Cell: ({ value }) =>
+  },
+  {
+    Header: "Start date",
+    accessor: "startDate",
+    Cell: ({ value }: { value: Date }) => value.toLocaleDateString(),
+  },
+]
+const data = [
+  {
+    id: "HBPM037",
+    patient: {
+      name: "Gregory C. Leonard",
+      ssn: "42318026",
     },
-    {
-      Header: "Referring physician",
-      accessor: "physician.name",
+    physician: {
+      name: "Allie Sanchez",
     },
-    {
-      Header: "Study status",
-      accessor: "status",
-      Cell: ({ value }: { value: "draft" | "pre-registered" | "pending" }) => {
-        return (
-          {
-            draft: "In Draft",
-            "pre-registered": "Pre-registered",
-            pending: "Pending report",
-          }[value] ?? ""
-        )
-      },
+    status: "draft",
+    startDate: new Date("12/23/2020"),
+  },
+  {
+    id: "HBPM036",
+    patient: {
+      name: "Patricia V. Carroll",
+      ssn: "547759769",
     },
-    {
-      Header: "Start date",
-      accessor: "startDate",
-      Cell: ({ value }: { value: Date }) => value.toLocaleDateString(),
+    physician: {
+      name: "Steven Nissen",
     },
-  ],
-  data: [
-    {
-      id: "HBPM037",
-      patient: {
-        name: "Gregory C. Leonard",
-        ssn: "42318026",
-      },
-      physician: {
-        name: "Allie Sanchez",
-      },
-      status: "draft",
-      startDate: new Date("12/23/2020"),
+    status: "pending",
+    startDate: new Date("12/23/2020"),
+  },
+  {
+    id: "HBPM035",
+    patient: {
+      name: "Glen A. Ortiz",
+      ssn: "609513889",
     },
-    {
-      id: "HBPM036",
-      patient: {
-        name: "Patricia V. Carroll",
-        ssn: "547759769",
-      },
-      physician: {
-        name: "Steven Nissen",
-      },
-      status: "pending",
-      startDate: new Date("12/23/2020"),
+    physician: {
+      name: "Allie Sanchez",
     },
-    {
+    status: "pre-registered",
+    startDate: new Date("12/22/2020"),
+  },
+  ...Array.from({ length: 50 })
+    .fill("")
+    .map(() => ({
       id: "HBPM035",
       patient: {
-        name: "Glen A. Ortiz",
-        ssn: "609513889",
+        name: `Glen A. Ortiz ${(Math.random() * 100).toFixed(0)}`,
+        ssn: `${(Math.random() * 100000000).toFixed(0)}`,
       },
       physician: {
         name: "Allie Sanchez",
       },
       status: "pre-registered",
       startDate: new Date("12/22/2020"),
-    },
-  ],
+    })),
+]
+
+Example.args = {
+  title: "Table Title",
+  columns,
+  data,
+  pageSize: 5,
+}
+
+const ControlledPaginationTemplate: Story<TableProps> = (props: TableProps) => {
+  const [pageSize, setPageSize] = useState(props.pageSize ?? 10)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const pageCount = useMemo(() => {
+    const size = props.data.length
+    return Math.ceil(size / pageSize)
+  }, [props.data, pageSize])
+
+  const paginatedData = useMemo(() => {
+    return props.data.slice(
+      (currentPage - 1) * pageSize,
+      currentPage * pageSize
+    )
+  }, [props.data, currentPage, pageSize])
+
+  return (
+    <Table
+      {...props}
+      data={paginatedData}
+      manualPagination
+      pageCount={pageCount}
+      pageSize={pageSize}
+      currentPage={currentPage}
+      totalCount={data.length}
+      onChangePagination={({ currentPage, pageSize }) => {
+        setPageSize(pageSize)
+        setCurrentPage(currentPage)
+      }}
+    />
+  )
+}
+export const ControlledPaginationExample = ControlledPaginationTemplate.bind({})
+
+ControlledPaginationExample.args = {
+  title: "Table Title",
+  columns,
+  data,
+  pageSize: 10,
 }
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,38 +1,96 @@
 /** @jsxRuntime classic /
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { useTable, Column } from "react-table"
-import { Pagination, Paper } from "../"
-
-// interface TableRowProps {
-//   children: React.ReactNode
-// }
-
-// const TableRow = ({ children }: TableRowProps) => {
-//   return <div>{children}</div>
-// }
+import { useTable, Column, usePagination, useFlexLayout } from "react-table"
+import { Pagination, Paper, DropdownSelect } from "../"
+import { memo, useEffect, useMemo } from "react"
 
 export interface TableProps {
   /** Table title */
   title?: string
 
-  /** Columns */
+  /** Definition of the table columns */
   columns: Column<Record<string, unknown>>[]
-  /** Data */
+  /** Data to be displayed in the table */
   data: Record<string, unknown>[]
+
+  /** If `true`, the table will be displayed in a loading state */
+  loading?: boolean
+
+  // filter
+  // sorting
+
+  /** If `true`, the table pagination will be controlled by the parent */
+  manualPagination?: boolean
+  /** Used if manualPagination is `true`. The total count of items in the table */
+  totalCount: number
+  /** Used if manualPagination is `true`. The total count of pages in the table */
+  pageCount?: number
+  /** Used if manualPagination is `true`. The current page being displayed */
+  currentPage?: number
+  /** If manualPagination is `true` it is the controlled page size of the table. If manualPagination is `false` it defines the initial pageSize of the table */
+  pageSize?: number
+  /** Callback called when the page size or current page of the table changes */
+  onChangePagination?: ({
+    pageSize,
+    currentPage,
+  }: {
+    pageSize: number
+    currentPage: number
+  }) => void
 }
 
-const Table = ({ columns, data, title }: TableProps) => {
+const Table = memo(function Table({
+  columns,
+  data,
+  title,
+  // loading,
+  manualPagination,
+  onChangePagination,
+  pageSize: controlledPageSize,
+  pageCount: controlledPageCount,
+  totalCount: controlledTotalCount,
+  currentPage: controlledPage,
+}: TableProps) {
   const {
     getTableBodyProps,
     getTableProps,
     headerGroups,
-    rows,
+    // rows,
+    page,
     prepareRow,
-  } = useTable({
-    columns,
-    data,
-  })
+    pageCount,
+    gotoPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      manualPagination,
+      ...(controlledPageCount ? { pageCount: controlledPageCount } : {}), // unexpected behaviour from react-table if pageCount is passed as undefined instead of not passed at all
+      initialState: {
+        pageSize: controlledPageSize ?? 10,
+        pageIndex: controlledPage ? controlledPage - 1 : 0,
+      },
+    },
+    usePagination,
+    useFlexLayout
+  )
+
+  useEffect(() => {
+    if (onChangePagination) {
+      onChangePagination({
+        pageSize,
+        currentPage: pageIndex + 1,
+      })
+    }
+  }, [onChangePagination, pageIndex, pageSize])
+
+  const totalCount = useMemo(() => {
+    if (manualPagination) return controlledTotalCount
+    return data.length
+  }, [manualPagination, controlledTotalCount, data])
 
   return (
     <Paper padding={0} elevation={1}>
@@ -45,79 +103,96 @@ const Table = ({ columns, data, title }: TableProps) => {
       >
         {title}
       </div>
-      <div>
-        <table
-          {...getTableProps()}
-          sx={{
-            width: "100%",
-            borderCollapse: "collapse",
-          }}
-        >
-          <thead>
-            {headerGroups.map((headerGroup) => (
-              // the spread has a 'key', but the eslint rule can't detect it
-              // eslint-disable-next-line react/jsx-key
-              <tr
-                {...headerGroup.getHeaderGroupProps()}
+
+      <div
+        {...getTableProps()}
+        sx={{
+          width: "100%",
+        }}
+      >
+        <div>
+          {headerGroups.map((headerGroup) => {
+            const {
+              key,
+              ...headerGroupProps
+            } = headerGroup.getHeaderGroupProps()
+            return (
+              <div
+                {...headerGroupProps}
+                key={key}
                 sx={{
                   borderBottom: "1px solid #E8E8E9", // TODO: use theme colors
                 }}
               >
-                {headerGroup.headers.map((column) => (
-                  // the spread has a 'key', but the eslint rule can't detect it
-                  // eslint-disable-next-line react/jsx-key
-                  <th
-                    {...column.getHeaderProps()}
-                    sx={{
-                      px: 6,
-                      pb: 3,
-                      color: "#777777", // TODO: use theme colors
-                      variant: "text.body1",
-                      textAlign: column.align ?? "start",
-                    }}
-                  >
-                    {column.render("Header")}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map((row) => {
-              prepareRow(row)
-              return (
-                // the spread has a 'key', but the eslint rule can't detect it
-                // eslint-disable-next-line react/jsx-key
-                <tr
-                  {...row.getRowProps()}
+                {headerGroup.headers.map((column) => {
+                  const { key, ...headerProps } = column.getHeaderProps()
+                  return (
+                    <div
+                      {...headerProps}
+                      key={key}
+                      sx={{
+                        px: 6,
+                        pb: 3,
+                        color: "#777777", // TODO: use theme colors
+                        variant: "text.body1",
+                        textAlign: column.align ?? "start",
+                      }}
+                    >
+                      {column.render("Header")}
+                    </div>
+                  )
+                })}
+              </div>
+            )
+          })}
+        </div>
+        <div {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row)
+            const { key, ...rowProps } = row.getRowProps()
+            return (
+              <div
+                key={key}
+                sx={{
+                  p: 1,
+                  borderBottom: "1px solid #E8E8E9", // TODO: use theme colors
+                }}
+              >
+                <div
+                  {...rowProps}
                   sx={{
-                    borderBottom: "1px solid #E8E8E9", // TODO: use theme colors
+                    borderRadius: 3,
+                    transition: "all 0.2s ease-out",
+                    "&:hover": {
+                      backgroundColor: "rgba(0, 130, 252, 0.06)",
+                    },
                   }}
                 >
                   {row.cells.map((cell) => {
+                    const { key, ...cellProps } = cell.getCellProps()
                     return (
-                      // the spread has a 'key', but the eslint rule can't detect it
-                      // eslint-disable-next-line react/jsx-key
-                      <td
-                        {...cell.getCellProps()}
+                      <div
+                        {...cellProps}
+                        key={key}
                         sx={{
-                          px: 6,
-                          py: 3,
+                          px: 5,
+                          py: 2,
                           color: cell.column.highlight ? "#0082FC" : "text", // TODO: use theme colors
                           variant: "text.body1",
                           textAlign: cell.column.align ?? "start",
                         }}
                       >
                         {cell.render("Cell")}
-                      </td>
+                      </div>
                     )
                   })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+                </div>
+              </div>
+            )
+          })}
+        </div>
       </div>
+
       <div
         sx={{
           display: "flex",
@@ -125,11 +200,33 @@ const Table = ({ columns, data, title }: TableProps) => {
           p: 6,
         }}
       >
-        <div>page size changer</div>
-        <Pagination />
+        <div
+          sx={{
+            display: "flex",
+            gap: 2,
+            alignItems: "center",
+            variant: "text.body1",
+            color: "muted",
+          }}
+        >
+          <span>Showing</span>
+          <DropdownSelect
+            options={["5", "10", "25", "50"]}
+            value={`${pageSize}`}
+            onChange={({ selectedItem }) =>
+              setPageSize(Number.parseInt(selectedItem ?? "10"))
+            }
+          />
+          <span>results per page from a total of {totalCount} results</span>
+        </div>
+        <Pagination
+          page={pageIndex + 1}
+          count={pageCount}
+          onChange={(page) => gotoPage(page - 1)}
+        />
       </div>
     </Paper>
   )
-}
+})
 
 export default Table
