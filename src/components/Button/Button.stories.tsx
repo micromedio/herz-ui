ca29a54f0b4068f502ba1c3d62aaf64a5f07bd56
Weliton Resende <welitonderesende@gmail.com>
2021-02-19 16:04:40 -0300

    DEV-14: insert new button component (#28)
    * DEV-14: Testing jira integration
    
    * DEV-14: insert new button component
    
    * DEV-14: Update theme-ui/colors to a version with types
    
    * DEV-14: update spacing to match theme-ui standard close #2 #1
    
    * DEV-14: create new text theme to match button design specs
    
    * Update src/components/Button/Button.tsx
    
    Update variant options
    
    Co-authored-by: Giuseppe Battistella <bartgb00@gmail.com>
    
    * DEV-14: update items from PR
    
    * DEV-14: Add styles only inside component itself
    
    * DEV-14: solve error on tests missing the theme Provider
    
    * DEV-14 Insert custom render separated from theme wrapper
    
    * add test/utils
    
    Co-authored-by: Giuseppe Battistella <bartgb00@gmail.com>

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react"
+import Button, { ButtonProps } from "./Button"
+import { action } from "@storybook/addon-actions"
+import { Meta, Story } from "@storybook/react/types-6-0"
+
+export default {
+  title: "Design System/Button",
+  component: Button,
+} as Meta
+
+const Template: Story<ButtonProps> = (props: ButtonProps) => {
+  return <Button {...props} />
+}
+
+export const Example = Template.bind({})
+Example.args = {
+  children: "Submit",
+  disabled: false,
+  variant: "filled",
+  color: "primary",
+  onClick: action("clicked"),
+}
+
+export const Filled = Template.bind({})
+Filled.args = {
+  ...Example.args,
+  variant: "filled",
+  color: "primary",
+}
+
+export const Plain = Template.bind({})
+Plain.args = {
+  ...Example.args,
+  variant: "plain",
+  color: "secondary",
+}
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,3 +1,82 @@
-import { Button } from "theme-ui"
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { Button as ThemeUIButton, jsx, SxStyleProp } from "theme-ui"
+import { MouseEvent, ButtonHTMLAttributes, forwardRef } from "react"
+import { shade } from "@theme-ui/color"
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children?: string
+
+  variant?: "filled" | "plain"
+
+  color?: string
+
+  disabled?: boolean
+
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
+}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button(
+  {
+    children,
+    onClick,
+    variant = "filled",
+    color = "primary",
+    disabled,
+    ...htmlProps
+  }: ButtonProps,
+  ref
+) {
+  const baseButton = {
+    variant: "text.button1",
+    borderRadius: 2,
+    cursor: "pointer",
+    position: "relative",
+    transition: "all .2s linear",
+  }
+
+  const filled: SxStyleProp = {
+    ...baseButton,
+    paddingX: 3,
+    paddingY: 2,
+    backgroundColor: color,
+    "&:hover": {
+      backgroundColor: shade(color, 0.1),
+    },
+    "&:disabled": {
+      opacity: 0.3,
+      cursor: "default",
+    },
+  }
+
+  const plain: SxStyleProp = {
+    ...baseButton,
+    paddingX: 1,
+    paddingY: 2,
+    backgroundColor: "transparent",
+    color: "#0082FC",
+    "&:hover": {
+      backgroundColor: "#0082FC0F",
+    },
+    "&:disabled": {
+      backgroundColor: "transparent",
+      opacity: 0.3,
+      cursor: "default",
+    },
+  }
+
+  return (
+    <ThemeUIButton
+      ref={ref}
+      onClick={onClick}
+      variant={variant}
+      {...htmlProps}
+      disabled={disabled}
+      sx={variant === "filled" ? filled : plain}
+    >
+      {children}
+    </ThemeUIButton>
+  )
+})
 
 export default Button
