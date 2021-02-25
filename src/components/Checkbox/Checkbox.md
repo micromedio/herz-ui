c232c364008ba787b84ff6ff881adc9ea6f17307
edaegonis <eduardocamposdev@gmail.com>
2021-02-25 19:52:20 -0300

    Add checkbox

---
+++ src/components/Checkbox/Checkbox.md
@@ -0,0 +1,17 @@
+Default Checkbox example:
+
+```js
+<Checkbox />
+```
+
+Labeled Checkbox example:
+
+```js
+<Checkbox label="I'm checked" checked={true} />
+```
+
+Indeterminate Checkbox state example:
+
+```js
+<Checkbox indeterminate={true} label="check me" />
+```
---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,155 @@
+/** @jsxRuntime classic /*
+/** @jsx jsx */
+import { jsx, Label } from "theme-ui"
+import * as React from "react"
+
+export interface ICheckboxProps {
+  checked: boolean
+  indeterminate: boolean
+  disabled: boolean
+  label: string
+  name: string
+  onChange(): Event
+}
+
+/** @TODO add colors to theme file */
+const stateStyles = {
+  resting: {
+    backgroundColor: "#F4F4F4",
+    borderColor: "transparent",
+  },
+  hover: {
+    backgroundColor: "#E8E8E9",
+    borderColor: "transparent",
+  },
+  active: {
+    backgroundColor: "#fff",
+    borderColor: "#0082FC",
+  },
+  filled: {
+    backgroundColor: "#0082FC",
+    borderColor: "transparent",
+  },
+}
+
+const indeterminateSvg = (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="20" height="20" rx="4" fill="#0082FC" />
+    <path
+      d="M6 10H10.2667H14"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+const checkedSvg = (
+  <svg
+    width="12"
+    height="12"
+    viewBox="0 0 12 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2.5 6L5 8.5L10 3.5"
+      stroke="white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default function Checkbox(props: ICheckboxProps) {
+  const {
+    checked = false,
+    disabled = false,
+    indeterminate = false,
+    label,
+    name,
+  } = props
+
+  return (
+    <React.Fragment>
+      <Label
+        htmlFor={name}
+        sx={{
+          position: "relative",
+          alignItems: "center",
+          "input:checked ~ &": {
+            color: "blue",
+          },
+        }}
+      >
+        <input
+          {...props}
+          type="checkbox"
+          id={name}
+          ref={(input) => {
+            if (input) {
+              input.checked = checked
+              input.indeterminate = indeterminate
+              input.disabled = disabled
+            }
+          }}
+          sx={{
+            position: "relative",
+            width: 20,
+            height: 20,
+            appearance: "none",
+            borderRadius: 1,
+            border: "2px solid transparent",
+            cursor: "pointer",
+            outline: "none",
+            transition: "all 0.2s",
+
+            ...(checked ? stateStyles.filled : stateStyles.resting),
+
+            "&:hover": {
+              ...(checked ? stateStyles.filled : stateStyles.hover),
+            },
+
+            "&:focus": {
+              ...(checked ? stateStyles.filled : stateStyles.active),
+            },
+
+            "&:checked": {
+              background: "#0082FC",
+            },
+
+            "&:checked ~ div, &:indeterminate ~ div": {
+              visibility: "visible",
+              opacity: 1,
+            },
+          }}
+        />
+        <div
+          sx={{
+            position: "absolute",
+            visibility: "hidden",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "20px",
+            width: "20px",
+            left: "0",
+            top: "0",
+            cursor: "pointer",
+          }}
+        >
+          {(indeterminate && indeterminateSvg) || (checked && checkedSvg)}
+        </div>
+        {label && label}
+      </Label>
+    </React.Fragment>
+  )
+}
