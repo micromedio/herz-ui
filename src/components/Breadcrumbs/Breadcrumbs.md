17615ae46744d46586010b4736970a7ec218074f
Giuseppe Battistella <bartgb00@gmail.com>
2021-02-25 16:39:33 -0300

    DEV-16: Added Breadcrumbs (#35)
    * DEV-16: Added breadcrumbs
    
    * DEV-16: removed commented code
    
    Co-authored-by: Giuseppe Battistella <giusbattistella@gmail.com>

---
+++ src/components/Breadcrumbs/Breadcrumbs.md
@@ -0,0 +1,15 @@
+The breadcrumbs component is used to display where the user is in the application at the moment and to provide an easy way to navigate back to the parent pages.
+
+### Usage
+
+Simply provide all the links as children, in the order they are supposed to be rendered.
+
+#### Example
+
+```jsx
+<Breadcrumbs>
+  <a href="#">Studies</a>
+  <a href="#">New Study</a>
+  <span>New Patient</span>
+</Breadcrumbs>
+```
---
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import Breadcrumbs from "./Breadcrumbs"
+import { axe } from "jest-axe"
+
+describe("Breadcrumbs", () => {
+  test("all children are rendered", () => {
+    // Arrange
+    render(
+      <Breadcrumbs>
+        <a href="#">Studies</a>
+        <a href="#">New Study</a>
+        <span>New Patient</span>
+      </Breadcrumbs>
+    )
+
+    // Assert
+    expect(screen.getByRole("link", { name: /studies/i })).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /new study/i })).toBeInTheDocument()
+    expect(screen.getByText(/new patient/i)).toBeInTheDocument()
+  })
+
+  test("passes a11y check", async () => {
+    // Arrange
+    const { container } = render(
+      <Breadcrumbs>
+        <a href="#">Studies</a>
+        <a href="#">New Study</a>
+        <span>New Patient</span>
+      </Breadcrumbs>
+    )
+    const results = await axe(container)
+
+    // Assert
+    expect(results).toHaveNoViolations()
+  })
+})
