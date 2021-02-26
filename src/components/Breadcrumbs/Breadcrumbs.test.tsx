f617dd7212b3a40d098c6affa779b5cce0a0a187
Giuseppe Battistella <giusbattistella@gmail.com>
2021-02-26 11:49:40 -0300

    DEV-56: Fixed tests

--- src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen } from "@testing-library/react"
+import { render, screen } from "../../tests/utils"
 import Breadcrumbs from "./Breadcrumbs"
 import { axe } from "jest-axe"
 
--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render } from "../../tests/utils"
 import { axe } from "jest-axe"
 import Pagination from "./Pagination"
 
