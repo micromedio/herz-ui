fcb682ee986a1c20a70345bbe7bd6300ea97b7da
Weliton Resende <welitonderesende@gmail.com>
2021-03-01 15:05:09 -0300

    DEV-42: Add icon component (#36)
    * DEV-42: Add icon component
    
    * DEV-42: Update implementation of Icon Comp
    
    * Update src/components/Icon/Icon.tsx
    
    Co-authored-by: Giuseppe Battistella <bartgb00@gmail.com>
    
    * DEV-42: adding some more description
    
    Co-authored-by: Giuseppe Battistella <bartgb00@gmail.com>

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,40 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+import * as TablerIcons from "@tabler/icons"
+
+type IconType = typeof import("@tabler/icons")
+
+export interface IconProps {
+  name: keyof IconType
+
+  // set custom `width` and `height`
+  size?: number
+
+  // set `stroke` color
+  color?: "primary" | "highlight" | "muted"
+
+  // set the thickness stroke
+  stroke?: number
+}
+
+const Icon = ({
+  name = "IconAlertTriangle",
+  size = 20,
+  color = "primary",
+  stroke = 2,
+}: IconProps) => {
+  const Component = TablerIcons[name]
+
+  return (
+    <Component
+      size={size}
+      stroke={stroke}
+      sx={{
+        color,
+      }}
+    />
+  )
+}
+
+export default Icon
