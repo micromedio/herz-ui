4e77e9d672889faf09d411ad6dcd27730b01e03c
Giuseppe Battistella <giusbattistella@gmail.com>
2021-03-15 18:36:55 -0300

    refactor: export component prop types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,29 @@
 export { default as Button } from "./Button/Button"
+export type { ButtonProps } from "./Button/Button"
+
 export { default as Paper } from "./Paper/Paper"
+export type { PaperProps } from "./Paper/Paper"
+
 export { default as Selector } from "./Selector/Selector"
+export type { SelectorProps } from "./Selector/Selector"
+
 export { default as Pagination } from "./Pagination/Pagination"
+export type { PaginationProps } from "./Pagination/Pagination"
+
 export { default as Tooltip } from "./Tooltip/Tooltip"
+export type { TooltipProps } from "./Tooltip/Tooltip"
+
 export { default as Table } from "./Table/Table"
+export type { TableProps } from "./Table/Table"
+
 export { default as TableFilters } from "./TableFilters/TableFilters"
+export type { TableFiltersProps } from "./TableFilters/TableFilters"
+
 export { default as TextField } from "./TextField/TextField"
+export type { TextFieldProps } from "./TextField/TextField"
+
 export { default as Input } from "./Input/Input"
+export type { InputProps } from "./Input/Input"
+
 export { default as Popover } from "./Popover/Popover"
+export type { PopoverProps } from "./Popover/Popover"
