61ed3f0420828d82db9e211417b62a84d75296e5
Giuseppe Battistella <giusbattistella@gmail.com>
2021-04-14 17:12:35 -0300

    DEV-87: Added Divider

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,19 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
+const Divider = () => {
+  return (
+    <hr
+      sx={{
+        border: "none",
+        height: "1px",
+        margin: 0,
+        flexShrink: 0,
+        backgroundColor: "text.90",
+      }}
+    />
+  )
+}
+
+export default Divider
