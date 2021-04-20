72b44f927a6af889b9811e7a43ff540b971754bf
Giuseppe Battistella <giusbattistella@gmail.com>
2021-04-20 11:32:32 -0300

    Refactor: Clip screenshots by default

--- src/tests/Storyshots.test.ts
+++ src/tests/Storyshots.test.ts
@@ -6,10 +6,12 @@ function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
+const fullPageComponents = ["Popover", "Tooltip", "FloatingCard", "Selector"]
+
 initStoryshots({
   suite: "Image storyshots",
   test: imageSnapshot({
-    async beforeScreenshot(page) {
+    async beforeScreenshot(page, options) {
       // disable animations and wait 1000ms for any transitions that already started to settle
       page.evaluate(() => {
         const css = `
@@ -23,6 +25,27 @@ initStoryshots({
         style.append(document.createTextNode(css))
       })
       await delay(1000)
+
+      for (const componentName of fullPageComponents) {
+        if (options.context.kind.startsWith(`Design System/${componentName}`)) {
+          return
+        }
+      }
+
+      const element = await page.$("#root > *")
+      if (element) return element
+    },
+    getScreenshotOptions: (options) => {
+      for (const componentName of fullPageComponents) {
+        if (options.context.kind.startsWith(`Design System/${componentName}`)) {
+          return {
+            fullPage: true,
+          }
+        }
+      }
+      return {
+        fullPage: false,
+      }
     },
     storybookUrl: `file://${path.resolve(__dirname, "../../storybook-static")}`,
     getMatchOptions: () => {
