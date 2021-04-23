35a5d2ea2d2cd2020e5a7c1856a649861bc5827b
Giuseppe Battistella <giusbattistella@gmail.com>
2021-04-23 11:51:10 -0300

    DEV-72: Fixed table row padding and borders

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -315,6 +315,8 @@ const Table = ({
                   <div
                     sx={{
                       width: 4,
+                      borderBottom: (theme: HerzUITheme) =>
+                        `1px solid ${theme.colors.text[90]}`,
                     }}
                   />
                 )}
@@ -339,7 +341,7 @@ const Table = ({
               >
                 <div
                   sx={{
-                    p: 1,
+                    p: 2,
                     minWidth: "fit-content",
                     borderBottom: (theme: HerzUITheme) =>
                       `1px solid ${theme.colors.text[90]}`,
@@ -442,6 +444,8 @@ const Table = ({
                   <div
                     sx={{
                       width: 4,
+                      borderBottom: (theme: HerzUITheme) =>
+                        `1px solid ${theme.colors.text[90]}`,
                       ...(activeRowIds[row.id]
                         ? {
                             backgroundColor: "primary.0",
