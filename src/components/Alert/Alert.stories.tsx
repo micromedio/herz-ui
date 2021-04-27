fd74d89011fad7833aa24eff6b6ea31023db10c9
Weliton de Resende <welitonderesende@gmail.com>
2021-04-27 15:51:49 -0300

    DEV-89 Add Alert component

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,56 @@
+import React from "react"
+import Alert, { AlertProps } from "./Alert"
+import { Meta, Story } from "@storybook/react/types-6-0"
+
+export default {
+  title: "Design System/Alert",
+  component: Alert,
+} as Meta
+
+const Template: Story<AlertProps> = (props) => <Alert {...props} />
+
+// Each story then reuses that template
+export const Example = Template.bind({})
+
+Example.args = {
+  title: "Alert going on",
+  children: "Here it goes your alert description",
+  iconName: "IconAlertOctagon",
+  color: "success",
+  position: "relative",
+}
+
+// Each story then reuses that template
+export const BigDescription = Template.bind({})
+
+BigDescription.args = {
+  ...Example.args,
+  children: (
+    <>
+      <span>Here it goes your alert description </span>
+      <p>yet here, more alert description </p>
+    </>
+  ),
+}
+
+export const CustomIcon = Template.bind({})
+
+CustomIcon.args = {
+  ...Example.args,
+  iconName: "",
+  color: "warning",
+  iconSVG: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+    >
+      <title>MS-SymbolLockup</title>
+      <rect x="1" y="1" width="9" height="9" fill="#f25022" />
+      <rect x="1" y="11" width="9" height="9" fill="#00a4ef" />
+      <rect x="11" y="1" width="9" height="9" fill="#7fba00" />
+      <rect x="11" y="11" width="9" height="9" fill="#ffb900" />
+    </svg>
+  ),
+}
---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,80 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import { ReactNode } from "react"
+import { jsx } from "theme-ui"
+import Icon, { IconProps } from "../Icon/Icon"
+import Paper from "../Paper/Paper"
+
+export interface AlertProps {
+  /** Alert title */
+  title: string
+
+  /** Description content */
+  children: string | ReactNode
+
+  /** The alert icon from tabler */
+  iconName?: IconProps["name"]
+
+  /** custom Alert icon */
+  iconSVG?: ReactNode
+
+  color: string
+
+  position: "fixed" | "relative"
+}
+
+const Alert = ({
+  title,
+  children,
+  iconName,
+  iconSVG,
+  color,
+  position = "relative",
+}: AlertProps) => {
+  return (
+    <Paper
+      elevation={0}
+      padding={0}
+      sx={
+        position === "relative"
+          ? {
+              display: "flex",
+              gap: 3,
+              p: 6,
+              border: "2px solid",
+              borderColor: `${color}.40`,
+              backgroundColor: `${color}.95`,
+              borderRadius: 3,
+            }
+          : {
+              display: "flex",
+              gap: 3,
+              p: 6,
+              position: "fixed",
+              top: 0,
+              left: 0,
+              width: "100%",
+              zIndex: 99,
+              borderBottom: "2px solid",
+              borderColor: `${color}.40`,
+              backgroundColor: `${color}.95`,
+              borderRadius: 0,
+            }
+      }
+    >
+      <span sx={{ width: 20, height: 20 }}>
+        {iconName ? <Icon name={iconName} /> : iconSVG ? iconSVG : ""}
+      </span>
+      <div>
+        <h2 sx={{ variant: "text.heading2", margin: 0, paddingBottom: 3 }}>
+          {title}
+        </h2>
+        <p sx={{ variant: "text.body1", margin: 0, color: "text.40" }}>
+          {children}
+        </p>
+      </div>
+    </Paper>
+  )
+}
+
+export default Alert
