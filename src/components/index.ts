eedde384ada44850f6af2ef8354ad0d37d60cb71
Vagner José Nicolodi <vagnernico@gmail.com>
2021-04-30 13:53:48 -0300

    DEV-86: Added the export on index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,46 @@
+export { default as Breadcrumbs } from "./Breadcrumbs/Breadcrumbs"
+export type { BreadcrumbsProps } from "./Breadcrumbs/Breadcrumbs"
+
 export { default as Button } from "./Button/Button"
 export type { ButtonProps } from "./Button/Button"
 
-export { default as Paper } from "./Paper/Paper"
-export type { PaperProps } from "./Paper/Paper"
+export { default as Card } from "./Card/Card"
+export type { CardProps } from "./Card/Card"
 
-export { default as Selector } from "./Selector/Selector"
-export type { SelectorProps } from "./Selector/Selector"
+export { default as Checkbox } from "./Checkbox/Checkbox"
+export type { ICheckboxProps as CheckboxProps } from "./Checkbox/Checkbox"
+
+export { default as Divider } from "./Divider/Divider"
+
+export { default as EditableText } from "./EditableText/EditableText"
+export type { EditableTextProps } from "./EditableText/EditableText"
+
+export { default as FloatingCard } from "./FloatingCard/FloatingCard"
+export type { FloatingCardProps } from "./FloatingCard/FloatingCard"
+
+export { default as Icon } from "./Icon/Icon"
+export type { IconProps } from "./Icon/Icon"
+
+export { default as Input } from "./Input/Input"
+export type { InputProps } from "./Input/Input"
+
+export { default as MobileModal } from "./MobileModal/MobileModal"
+export type { MobileModalProps } from "./MobileModal/MobileModal"
 
 export { default as Pagination } from "./Pagination/Pagination"
 export type { PaginationProps } from "./Pagination/Pagination"
 
-export { default as Tooltip } from "./Tooltip/Tooltip"
-export type { TooltipProps } from "./Tooltip/Tooltip"
+export { default as Paper } from "./Paper/Paper"
+export type { PaperProps } from "./Paper/Paper"
+
+export { default as Popover } from "./Popover/Popover"
+export type { PopoverProps } from "./Popover/Popover"
+
+export { default as Selector } from "./Selector/Selector"
+export type { SelectorProps } from "./Selector/Selector"
+
+export { default as Spinner } from "./Spinner/Spinner"
+export type { SpinnerProps } from "./Spinner/Spinner"
 
 export { default as Table } from "./Table/Table"
 export type { TableProps } from "./Table/Table"
@@ -22,40 +51,14 @@ export type { TableFiltersProps } from "./TableFilters/TableFilters"
 export { default as TextField } from "./TextField/TextField"
 export type { TextFieldProps } from "./TextField/TextField"
 
-export { default as Input } from "./Input/Input"
-export type { InputProps } from "./Input/Input"
-
-export { default as Popover } from "./Popover/Popover"
-export type { PopoverProps } from "./Popover/Popover"
-
-export { default as Icon } from "./Icon/Icon"
-export type { IconProps } from "./Icon/Icon"
-
-export { default as FloatingCard } from "./FloatingCard/FloatingCard"
-export type { FloatingCardProps } from "./FloatingCard/FloatingCard"
-
-export { default as Checkbox } from "./Checkbox/Checkbox"
-export type { ICheckboxProps as CheckboxProps } from "./Checkbox/Checkbox"
-
-export { default as Card } from "./Card/Card"
-export type { CardProps } from "./Card/Card"
-
-export { default as Breadcrumbs } from "./Breadcrumbs/Breadcrumbs"
-export type { BreadcrumbsProps } from "./Breadcrumbs/Breadcrumbs"
-
 export { default as ThemeWrapper } from "../theme/ThemeWrapper"
 export type { IThemeWrapperProps as ThemeWrapperProps } from "../theme/ThemeWrapper"
+
+export { default as Tooltip } from "./Tooltip/Tooltip"
+export type { TooltipProps } from "./Tooltip/Tooltip"
 
 export { default as Uploader } from "./Uploader/Uploader"
 export type { IUploaderProps as UploaderProps } from "./Uploader/Uploader"
 
-export { default as EditableText } from "./EditableText/EditableText"
-export type { EditableTextProps } from "./EditableText/EditableText"
-
 export { default as ValueList } from "./ValueList/ValueList"
 export type { ValueListProps } from "./ValueList/ValueList"
-
-export { default as Spinner } from "./Spinner/Spinner"
-export type { SpinnerProps } from "./Spinner/Spinner"
-
-export { default as Divider } from "./Divider/Divider"
