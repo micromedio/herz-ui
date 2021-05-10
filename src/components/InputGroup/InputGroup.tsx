2af437ea7ed4e5032ed69f94195f90886e437baf
Giuseppe Battistella <giusbattistella@gmail.com>
2021-05-10 17:48:56 -0300

    feat(InputGroup): Added InputGroup component DEV-137

---
+++ src/components/InputGroup/InputGroup.tsx
@@ -0,0 +1,35 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import React from "react"
+import { jsx } from "theme-ui"
+import { InputGroupContext } from "./Context"
+
+export interface InputGroupProps {
+  children: React.ReactNode
+}
+
+const InputGroup = ({ children }: InputGroupProps) => {
+  const totalInputs = React.Children.count(children)
+
+  return (
+    <div sx={{ display: "flex" }}>
+      {React.Children.map(children, (child, index) => {
+        if (!React.isValidElement(child)) return null
+
+        return (
+          <InputGroupContext.Provider
+            value={{
+              index,
+              isFirst: index === 0,
+              isLast: index === totalInputs - 1,
+            }}
+          >
+            {child}
+          </InputGroupContext.Provider>
+        )
+      })}
+    </div>
+  )
+}
+
+export default InputGroup
