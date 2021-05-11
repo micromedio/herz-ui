57bd59c4471bb90c47df58e1e00eb1a721bcc801
Giuseppe Battistella <giusbattistella@gmail.com>
2021-05-11 18:42:55 -0300

    feat(Select): Added new Select component DEV-53

---
+++ src/components/Select/Select.tsx
@@ -0,0 +1,353 @@
+/** @jsxRuntime classic /*
+/** @jsx jsx */
+import React, { useContext, useMemo } from "react"
+import { HerzUITheme, jsx } from "theme-ui"
+
+import { useSelect, SELECT_BULK_ACTIONS } from "./hooks/useSelect"
+import { Button, Popover, Icon } from ".."
+import { InputGroupContext } from "../InputGroup/Context"
+import { getDataFromChildren, isArrayEqual } from "./utils"
+import { SelectOption } from "./SelectOption"
+import { SelectContext } from "./context"
+
+export type SelectValue = string | number | Record<string, unknown>
+export type SelectedItems = Array<SelectValue>
+
+export type SelectOption = {
+  value: SelectValue
+  label: React.ReactNode
+}
+
+export interface SelectProps {
+  /** The id of the Select. Use this prop to make label and `helperText` accessible for screen readers */
+  id?: string
+  /** Label text to be placed before the element */
+  label?: string
+  /** The placeholder text, shown when there is no selected value */
+  placeholder?: string
+  /** The value of the `select` element, required for a controlled component */
+  value?: SelectValue
+  /** Default value which will not trigger the `filled` select state */
+  defaultValue?: SelectValue
+  /** Whether the component is disabled or not */
+  disabled?: boolean
+  /** Whether the user can select multiple options or not */
+  multi?: boolean
+  /** Current selected items for multiple selection */
+  selectedItems?: SelectedItems
+  /** Default array of selected items which will not trigger the `filled` select state */
+  defaultSelectedItems?: SelectedItems
+  /** Callback fired when the value is changed */
+  onChange?: (changes: SelectValue) => void
+  /** Callback fired when the selected items change for multiple selection */
+  onSelectedItemsChange?: (changes: SelectedItems) => void
+  /** Highlight the select when it's in a `filled` state */
+  hightlightFilled?: boolean
+  /** Select grows to fill the width of the parent */
+  fullWidth?: boolean
+  children: React.ReactNode
+}
+
+/**
+ * Component responsible for rendering a select dropdown from given options
+ */
+const Select = ({
+  id,
+  label,
+  value,
+  defaultValue,
+  disabled = false,
+  multi = false,
+  placeholder,
+  selectedItems = [],
+  defaultSelectedItems,
+  onChange,
+  onSelectedItemsChange,
+  hightlightFilled = true,
+  fullWidth,
+  children,
+}: SelectProps) => {
+  const inputGroupContext = useContext(InputGroupContext)
+  const isGrouped = !!inputGroupContext
+
+  const options = useMemo(() => {
+    return getDataFromChildren(children)
+  }, [children])
+
+  const {
+    isOpen,
+    selectedItem,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+    getDropdownProps,
+    handleBulkAction,
+  } = useSelect({
+    value,
+    options,
+    multi,
+    selectedItems,
+    onChange,
+    onSelectedItemsChange,
+  })
+
+  const stateStyles = {
+    resting: {
+      backgroundColor: "text.alpha.95",
+      color: "text.40",
+      boxShadow: "unset",
+      borderColor: "transparent",
+    },
+    hover: {
+      backgroundColor: "text.alpha.90",
+      color: "text.0",
+      boxShadow: "unset",
+      borderColor: "transparent",
+    },
+    active: {
+      backgroundColor: "#fff",
+      color: "text.0",
+      boxShadow: (theme: HerzUITheme) =>
+        `0px 0px 0px 4px ${theme.colors.secondary.alpha[95]}`,
+      borderColor: "secondary.0",
+      fontWeight: "semibold",
+    },
+    filled: {
+      backgroundColor: "secondary.alpha.90",
+      color: "text.0",
+      boxShadow: "unset",
+      ...(hightlightFilled
+        ? {
+            borderColor: "secondary.0",
+            fontWeight: "semibold",
+          }
+        : {}),
+    },
+    filledHover: {
+      backgroundColor: "secondary.alpha.85",
+    },
+  }
+
+  const getMultiSelectLabel = () => {
+    if (selectedItems.length > 0) {
+      if (selectedItems.length === options.length) {
+        return "All"
+      }
+      if (selectedItems.length === 1) {
+        return options.find(({ value }) => value === selectedItems[0])?.label
+      }
+      return selectedItems.length + " selected"
+    }
+
+    return placeholder || "Select one or more options"
+  }
+
+  const selectedOption = useMemo(() => {
+    return (
+      (selectedItem && options.find(({ value }) => value === selectedItem)) ||
+      undefined
+    )
+  }, [options, selectedItem])
+
+  const areInitialItemsSelected = useMemo(() => {
+    return (
+      defaultSelectedItems && isArrayEqual(defaultSelectedItems, selectedItems)
+    )
+  }, [defaultSelectedItems, selectedItems])
+
+  const isInitialValueSelected = defaultValue && defaultValue === selectedItem
+
+  const isSelectFilled =
+    !areInitialItemsSelected &&
+    !isInitialValueSelected &&
+    (selectedItem || selectedItems.length > 0)
+
+  const hoverStyles = useMemo(() => {
+    if (disabled) return {}
+    if (isOpen) return stateStyles.active
+    if (isSelectFilled) return stateStyles.filledHover
+    return stateStyles.hover
+  }, [
+    disabled,
+    isOpen,
+    isSelectFilled,
+    stateStyles.active,
+    stateStyles.filledHover,
+    stateStyles.hover,
+  ])
+
+  return (
+    <div
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        position: "relative",
+        opacity: disabled ? 0.3 : 1,
+        variant: "text.body1",
+        ...(isGrouped && {
+          ...(!inputGroupContext?.isLast && {
+            borderRight: "1px solid",
+            borderColor: "text.90",
+            flexGrow: 0,
+          }),
+        }),
+      }}
+    >
+      {label && (
+        <label
+          sx={{
+            marginRight: 2,
+            fontSize: 14,
+            color: "text.40",
+            ...(!disabled ? { cursor: "pointer" } : {}),
+          }}
+          {...getLabelProps({ disabled, htmlFor: id })}
+        >
+          {label}
+        </label>
+      )}
+
+      <Popover
+        isVisible={isOpen}
+        isInteractive
+        placement="bottom-start"
+        noPadding
+        content={
+          <div
+            {...getMenuProps(
+              {
+                disabled,
+              },
+              {
+                suppressRefError: true,
+              }
+            )}
+            sx={{
+              maxHeight: 350,
+              overflowY: "auto",
+              padding: 4,
+              outline: 0,
+              margin: 0,
+              marginTop: 1,
+              listStyle: "none",
+            }}
+          >
+            {React.Children.map(children, (child, index) => {
+              if (!React.isValidElement(child)) return null
+
+              return (
+                <SelectContext.Provider
+                  value={{
+                    index,
+                    highlightedIndex,
+                    selectedItem,
+                    selectedItems,
+                    multi,
+                    getItemProps,
+                  }}
+                >
+                  {child}
+                </SelectContext.Provider>
+              )
+            })}
+
+            {multi && (
+              <Button
+                variant="plain"
+                color="secondary"
+                sx={{
+                  cursor: "pointer",
+                  width: "100%",
+                  justifyContent: "start",
+                }}
+                onClick={() => {
+                  if (isSelectFilled) {
+                    handleBulkAction(SELECT_BULK_ACTIONS.DESELECT_ALL)
+                    return true
+                  }
+
+                  handleBulkAction(SELECT_BULK_ACTIONS.SELECT_ALL)
+                }}
+              >
+                {isSelectFilled ? "Deselect all" : "Select all"}
+              </Button>
+            )}
+            <Button
+              variant="plain"
+              color="secondary"
+              sx={{
+                cursor: "pointer",
+                width: "100%",
+                justifyContent: "start",
+              }}
+              // onClick={() => {
+              //   if (isSelectFilled) {
+              //     handleBulkAction(SELECT_BULK_ACTIONS.DESELECT_ALL)
+              //     return true
+              //   }
+
+              //   handleBulkAction(SELECT_BULK_ACTIONS.SELECT_ALL)
+              // }}
+            >
+              Custom...
+            </Button>
+          </div>
+        }
+      >
+        <button
+          sx={{
+            display: "flex",
+            gap: 2,
+            borderRadius: 2,
+            height: [40, 36],
+            paddingX: 3,
+            justifyContent: "space-between",
+            alignItems: "center",
+            outline: 0,
+            border: "2px solid transparent",
+            transition: "all .2s linear",
+
+            ...(isGrouped && {
+              ...(!inputGroupContext?.isFirst && {
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+              }),
+              ...(!inputGroupContext?.isLast && {
+                borderTopRightRadius: 0,
+                borderBottomRightRadius: 0,
+              }),
+            }),
+
+            ...(fullWidth ? { flexGrow: 1 } : {}),
+            ...(isSelectFilled ? stateStyles.filled : stateStyles.resting),
+            ...(!disabled && { cursor: "pointer" }),
+
+            "&:hover": hoverStyles,
+            ...(isOpen && stateStyles.active),
+          }}
+          type="button"
+          {...getToggleButtonProps({
+            id,
+            "aria-labelledby": "",
+            ...getDropdownProps({ preventKeyAction: isOpen }),
+            disabled,
+          })}
+        >
+          <span>
+            {multi
+              ? getMultiSelectLabel()
+              : (selectedItem && selectedOption?.label) ||
+                (placeholder !== undefined && placeholder) ||
+                "Select an option"}
+          </span>
+          <Icon name="IconChevronDown" size={12} stroke={3} />
+        </button>
+      </Popover>
+    </div>
+  )
+}
+
+Select.Option = SelectOption
+export default Select
