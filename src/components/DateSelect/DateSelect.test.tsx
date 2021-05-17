d278894c0ba3678d8ea29a0cf474c24921ef32f8
Giuseppe Battistella <giusbattistella@gmail.com>
2021-05-17 10:17:07 -0300

    test(DateSelect): Added tests DEV-53

---
+++ src/components/DateSelect/DateSelect.test.tsx
@@ -0,0 +1,151 @@
+import { render, screen, fireEvent } from "../../tests/utils"
+import DateSelect from "./DateSelect"
+import userEvent from "@testing-library/user-event"
+import { axe } from "jest-axe"
+
+describe("DateSelect", () => {
+  test("date options appear on click", async () => {
+    render(
+      <DateSelect>
+        <DateSelect.Option value={{ from: "12/05/2021", to: "12/05/2021" }}>
+          Today
+        </DateSelect.Option>
+        <DateSelect.Option value={{ from: "13/05/2021", to: "13/05/2021" }}>
+          Tomorrow
+        </DateSelect.Option>
+      </DateSelect>
+    )
+
+    expect(screen.queryByText(/today/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/tomorrow/i)).not.toBeInTheDocument()
+    expect(screen.queryByText(/custom/i)).not.toBeInTheDocument()
+    userEvent.click(screen.getByText(/select an option/i))
+    expect(screen.getByText(/today/i)).toBeInTheDocument()
+    expect(screen.getByText(/tomorrow/i)).toBeInTheDocument()
+    expect(screen.getByText(/custom/i)).toBeInTheDocument()
+  })
+
+  test("onChange is called when an option is selected", async () => {
+    const onChange = jest.fn()
+    render(
+      <DateSelect onChange={onChange}>
+        <DateSelect.Option value={{ from: "12/05/2021", to: "12/05/2021" }}>
+          Today
+        </DateSelect.Option>
+      </DateSelect>
+    )
+
+    userEvent.click(screen.getByText(/select an option/i))
+    userEvent.click(screen.getByText(/today/i))
+    expect(onChange).toHaveBeenCalledWith({
+      from: "12/05/2021",
+      to: "12/05/2021",
+    })
+  })
+
+  test("custom date range popover appears when custom option is clicked", async () => {
+    render(
+      <DateSelect>
+        <DateSelect.Option value={{ from: "12/05/2021", to: "12/05/2021" }}>
+          Today
+        </DateSelect.Option>
+      </DateSelect>
+    )
+
+    userEvent.click(screen.getByText(/select an option/i))
+    expect(
+      screen.queryByRole("textbox", { name: /to/i })
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole("textbox", { name: /from/i })
+    ).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole("button", { name: /set/i })
+    ).not.toBeInTheDocument()
+    userEvent.click(screen.getByText(/custom/i))
+    expect(screen.getByRole("textbox", { name: /to/i })).toBeInTheDocument()
+    expect(screen.getByRole("textbox", { name: /from/i })).toBeInTheDocument()
+    expect(screen.getByRole("button", { name: /set/i })).toBeInTheDocument()
+  })
+
+  test("custom date cannot be set if 'from' date is after 'to' date", async () => {
+    render(
+      <DateSelect>
+        <DateSelect.Option value={{ from: "12/05/2021", to: "12/05/2021" }}>
+          Today
+        </DateSelect.Option>
+      </DateSelect>
+    )
+
+    userEvent.click(screen.getByText(/select an option/i))
+    userEvent.click(screen.getByText(/custom/i))
+    const fromInput = screen.getByRole("textbox", { name: /from/i })
+    const toInput = screen.getByRole("textbox", { name: /to/i })
+    const setButton = screen.getByRole("button", { name: /set/i })
+    expect(setButton).toBeDisabled()
+
+    // default format is MM/dd/yyyy
+    fireEvent.change(fromInput, { target: { value: "01/01/2020" } })
+    fireEvent.change(toInput, { target: { value: "01/01/2010" } })
+    expect(setButton).toBeDisabled()
+  })
+
+  test("onChange is called with custom date", async () => {
+    const onChange = jest.fn()
+    render(
+      <DateSelect onChange={onChange}>
+        <DateSelect.Option value={{ from: "12/05/2021", to: "12/05/2021" }}>
+          Today
+        </DateSelect.Option>
+      </DateSelect>
+    )
+
+    userEvent.click(screen.getByText(/select an option/i))
+    userEvent.click(screen.getByText(/custom/i))
+    const fromInput = screen.getByRole("textbox", { name: /from/i })
+    const toInput = screen.getByRole("textbox", { name: /to/i })
+    const setButton = screen.getByRole("button", { name: /set/i })
+    expect(setButton).toBeDisabled()
+
+    // default format is MM/dd/yyyy
+    fireEvent.change(fromInput, { target: { value: "05/09/2020" } })
+    fireEvent.change(toInput, { target: { value: "02/03/2022" } })
+    expect(setButton).not.toBeDisabled()
+    expect(onChange).not.toHaveBeenCalled()
+    userEvent.click(setButton)
+    expect(onChange).toHaveBeenCalledWith({
+      from: "05/09/2020",
+      to: "02/03/2022",
+    })
+  })
+
+  test("passes a11y check", async () => {
+    // Arrange
+    const { container } = render(
+      <DateSelect>
+        <DateSelect.Option value={{ from: "12/05/2021", to: "12/05/2021" }}>
+          Today
+        </DateSelect.Option>
+        <DateSelect.Option value={{ from: "13/05/2021", to: "13/05/2021" }}>
+          Tomorrow
+        </DateSelect.Option>
+        <DateSelect.Option value={{ from: "09/05/2021", to: "15/05/2021" }}>
+          This week
+        </DateSelect.Option>
+        <DateSelect.Option value={{ from: "16/05/2021", to: "22/05/2021" }}>
+          Next week
+        </DateSelect.Option>
+        <DateSelect.Option value={{ from: "01/05/2021", to: "31/05/2021" }}>
+          This month
+        </DateSelect.Option>
+        <DateSelect.Option value={{ from: "01/05/2021", to: "30/05/2021" }}>
+          Next month
+        </DateSelect.Option>
+      </DateSelect>
+    )
+    const results = await axe(container)
+
+    // Assert
+    expect(results).toHaveNoViolations()
+  })
+})
