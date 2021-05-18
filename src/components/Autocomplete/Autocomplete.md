2e28fa6219797e25fdb2954a57e45af734043e52
Vagner José Nicolodi <vagnernico@gmail.com>
2021-05-18 10:36:52 -0300

    feat(Autocomplete): Add Autocomplete component DEV-152

---
+++ src/components/Autocomplete/Autocomplete.md
@@ -0,0 +1,394 @@
+Autocomplete example:
+
+#### Default
+
+```js
+const mockedOptions = [
+  { value: 1, label: "Neptunium", symbol: "Np", A: 237, Z: 93 },
+  { value: 2, label: "Plutonium", symbol: "Pu", A: 244, Z: 94 },
+  { value: 3, label: "Americium Darmstad", symbol: "Am", A: 243, Z: 95 },
+  { value: 4, label: "Curium", symbol: "Cm", A: 247, Z: 96 },
+  { value: 5, label: "Berkelium Flerovios", symbol: "Bk", A: 247, Z: 97 },
+  { value: 6, label: "Californium Copernicus", symbol: "Cf", A: 251, Z: 98 },
+  { value: 7, label: "Einsteinium", symbol: "Es", A: 252, Z: 99 },
+  { value: 8, label: "Fermium", symbol: "Fm", A: 257, Z: 100 },
+  { value: 9, label: "Mendelevium", symbol: "Md", A: 258, Z: 101 },
+  { value: 10, label: "Nobelium", symbol: "No", A: 259, Z: 102 },
+]
+const [items, setItems] = React.useState(mockedOptions)
+const [value, setValue] = React.useState(null)
+;(
+<Autocomplete
+  onInputValueChange={({ inputValue }) => {
+    setItems(
+      mockedOptions.filter((option) =>
+        inputValue
+          ? option.label
+              .toLocaleLowerCase()
+              .startsWith(inputValue.toLocaleLowerCase())
+          : true
+      )
+    )
+  }}
+  options={items}
+  optionToString={(option) => (option ? option.label : "")}
+  placeholder="Search by organization's name or handle"
+  renderOption={(_highlightedIndex, defaultStyles, option) => (
+    <div
+      style={{
+        ...defaultStyles,
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: defaultStyles.backgroundColor && "rgba(0,130,252,0.05)",
+        display: "flex",
+        padding: 2,
+      }}
+    >
+      {option.label}
+    </div>
+  )}
+  onSelectedItemChange={({ selectedItem: selectedOption }) => {
+    setValue(selectedOption)
+  }}
+  selectedOption={value}
+  totalCount={mockedOptions.length}
+/>
+)
+```
+
+#### With Label And Helper Text
+
+```js
+const mockedOptions = [
+  { value: 1, label: "Neptunium", symbol: "Np", A: 237, Z: 93 },
+  { value: 2, label: "Plutonium", symbol: "Pu", A: 244, Z: 94 },
+  { value: 3, label: "Americium Darmstad", symbol: "Am", A: 243, Z: 95 },
+  { value: 4, label: "Curium", symbol: "Cm", A: 247, Z: 96 },
+  { value: 5, label: "Berkelium Flerovios", symbol: "Bk", A: 247, Z: 97 },
+  { value: 6, label: "Californium Copernicus", symbol: "Cf", A: 251, Z: 98 },
+  { value: 7, label: "Einsteinium", symbol: "Es", A: 252, Z: 99 },
+  { value: 8, label: "Fermium", symbol: "Fm", A: 257, Z: 100 },
+  { value: 9, label: "Mendelevium", symbol: "Md", A: 258, Z: 101 },
+  { value: 10, label: "Nobelium", symbol: "No", A: 259, Z: 102 },
+]
+const [items, setItems] = React.useState(mockedOptions)
+const [value, setValue] = React.useState(null)
+;(
+<Autocomplete
+  helperText="This is a helper text"
+  label="Label"
+  onInputValueChange={({ inputValue }) => {
+    setItems(
+      mockedOptions.filter((option) =>
+        inputValue
+          ? option.label
+              .toLocaleLowerCase()
+              .startsWith(inputValue.toLocaleLowerCase())
+          : true
+      )
+    )
+  }}
+  optionalText="optional"
+  options={items}
+  optionToString={(option) => (option ? option.label : "")}
+  placeholder="Search by organization's name or handle"
+  renderOption={(highlightedIndex, defaultStyles, option) => (
+    <div
+      style={{
+        ...defaultStyles,
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: defaultStyles.backgroundColor && "rgba(0,130,252,0.05)",
+        display: "flex",
+        padding: 2,
+      }}
+    >
+      {option.label}
+    </div>
+  )}
+  onSelectedItemChange={({ selectedItem: selectedOption }) => {
+    setValue(selectedOption)
+  }}
+  selectedOption={value}
+  totalCount={mockedOptions.length}
+/>
+)
+```
+
+#### With Action Buttons
+
+```js
+const mockedOptions = [
+  { value: 1, label: "Neptunium", symbol: "Np", A: 237, Z: 93 },
+  { value: 2, label: "Plutonium", symbol: "Pu", A: 244, Z: 94 },
+  { value: 3, label: "Americium Darmstad", symbol: "Am", A: 243, Z: 95 },
+  { value: 4, label: "Curium", symbol: "Cm", A: 247, Z: 96 },
+  { value: 5, label: "Berkelium Flerovios", symbol: "Bk", A: 247, Z: 97 },
+  { value: 6, label: "Californium Copernicus", symbol: "Cf", A: 251, Z: 98 },
+  { value: 7, label: "Einsteinium", symbol: "Es", A: 252, Z: 99 },
+  { value: 8, label: "Fermium", symbol: "Fm", A: 257, Z: 100 },
+  { value: 9, label: "Mendelevium", symbol: "Md", A: 258, Z: 101 },
+  { value: 10, label: "Nobelium", symbol: "No", A: 259, Z: 102 },
+]
+const [items, setItems] = React.useState(mockedOptions)
+const [value, setValue] = React.useState(mockedOptions[0])
+;(
+<Autocomplete
+  buttons={
+    [
+      {
+        color: "secondary",
+        children: "More info",
+        onClick: () => {
+          alert("Clicked on more info!")
+        },
+        variant: "plain",
+      },
+      {
+        color: "secondary",
+        children: "Clear",
+        onClick: () => {
+          setValue(null)
+        },
+        variant: "plain",
+      },
+    ]
+  }
+  helperText="This is a helper text"
+  label="Label"
+  onInputValueChange={({ inputValue }) => {
+    setItems(
+      mockedOptions.filter((option) =>
+        inputValue
+          ? option.label
+              .toLocaleLowerCase()
+              .startsWith(inputValue.toLocaleLowerCase())
+          : true
+      )
+    )
+  }}
+  optionalText="optional"
+  options={items}
+  optionToString={(option) => (option ? option.label : "")}
+  placeholder="Search by organization's name or handle"
+  renderOption={(highlightedIndex, defaultStyles, option) => (
+    <div
+      style={{
+        ...defaultStyles,
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: defaultStyles.backgroundColor && "rgba(0,130,252,0.05)",
+        display: "flex",
+        padding: 2,
+      }}
+    >
+      {option.label}
+    </div>
+  )}
+  onSelectedItemChange={({ selectedItem: selectedOption }) => {
+    setValue(selectedOption)
+  }}
+  selectedOption={value}
+  totalCount={mockedOptions.length}
+/>
+)
+```
+#### With Custom Selected Option Render
+
+```js
+const mockedOptions = [
+  { value: 1, label: "Neptunium", symbol: "Np", A: 237, Z: 93 },
+  { value: 2, label: "Plutonium", symbol: "Pu", A: 244, Z: 94 },
+  { value: 3, label: "Americium Darmstad", symbol: "Am", A: 243, Z: 95 },
+  { value: 4, label: "Curium", symbol: "Cm", A: 247, Z: 96 },
+  { value: 5, label: "Berkelium Flerovios", symbol: "Bk", A: 247, Z: 97 },
+  { value: 6, label: "Californium Copernicus", symbol: "Cf", A: 251, Z: 98 },
+  { value: 7, label: "Einsteinium", symbol: "Es", A: 252, Z: 99 },
+  { value: 8, label: "Fermium", symbol: "Fm", A: 257, Z: 100 },
+  { value: 9, label: "Mendelevium", symbol: "Md", A: 258, Z: 101 },
+  { value: 10, label: "Nobelium", symbol: "No", A: 259, Z: 102 },
+]
+const [items, setItems] = React.useState(mockedOptions)
+const [value, setValue] = React.useState(mockedOptions[5])
+;(
+<Autocomplete
+  buttons={
+    [
+      {
+        color: "secondary",
+        children: "More info",
+        onClick: () => {
+          alert("Clicked on more info!")
+        },
+        variant: "plain",
+      },
+      {
+        color: "secondary",
+        children: "Clear",
+        onClick: () => {
+          setValue(null)
+        },
+        variant: "plain",
+      },
+    ]
+  }
+  helperText="This is a helper text"
+  label="Label"
+  onInputValueChange={({ inputValue }) => {
+    setItems(
+      mockedOptions.filter((option) =>
+        inputValue
+          ? option.label
+              .toLocaleLowerCase()
+              .startsWith(inputValue.toLocaleLowerCase())
+          : true
+      )
+    )
+  }}
+  optionalText="optional"
+  options={items}
+  optionToString={(option) => (option ? option.label : "")}
+  placeholder="Search by organization's name or handle"
+  renderOption={(highlightedIndex, defaultStyles, option) => (
+    <div
+      style={{
+        ...defaultStyles,
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: defaultStyles.backgroundColor && "rgba(0,130,252,0.05)",
+        display: "flex",
+        padding: 2,
+      }}
+    >
+      {option.label}
+    </div>
+  )}
+  renderSelectedItem={(selectedOption) => {
+    return (
+      <span>
+        {selectedOption.label}{" "}
+        <strong>
+          <sup style={{ position: "relative", left: 12, top: -5 }}>
+            {selectedOption.A}
+          </sup>
+          <sub
+            style={{
+              position: "relative",
+              top: 5,
+            }}
+          >
+            {selectedOption.Z}
+          </sub>
+          {selectedOption.symbol}
+        </strong>
+      </span>
+    )
+  }}
+  onSelectedItemChange={({ selectedItem: selectedOption }) => {
+    setValue(selectedOption)
+  }}
+  selectedOption={value}
+  totalCount={mockedOptions.length}
+/>
+)
+```
+#### With Highlight
+
+```js
+import Highlight from "../Highlight/Highlight"
+const mockedOptions = [
+  { value: 1, label: "Neptunium", symbol: "Np", A: 237, Z: 93 },
+  { value: 2, label: "Plutonium", symbol: "Pu", A: 244, Z: 94 },
+  { value: 3, label: "Americium Darmstad", symbol: "Am", A: 243, Z: 95 },
+  { value: 4, label: "Curium", symbol: "Cm", A: 247, Z: 96 },
+  { value: 5, label: "Berkelium Flerovios", symbol: "Bk", A: 247, Z: 97 },
+  { value: 6, label: "Californium Copernicus", symbol: "Cf", A: 251, Z: 98 },
+  { value: 7, label: "Einsteinium", symbol: "Es", A: 252, Z: 99 },
+  { value: 8, label: "Fermium", symbol: "Fm", A: 257, Z: 100 },
+  { value: 9, label: "Mendelevium", symbol: "Md", A: 258, Z: 101 },
+  { value: 10, label: "Nobelium", symbol: "No", A: 259, Z: 102 },
+]
+const [items, setItems] = React.useState(mockedOptions)
+const [value, setValue] = React.useState(null)
+;(
+<Autocomplete
+  buttons={
+    [
+      {
+        color: "secondary",
+        children: "More info",
+        onClick: () => {
+          alert("Clicked on more info!")
+        },
+        variant: "plain",
+      },
+      {
+        color: "secondary",
+        children: "Clear",
+        onClick: () => {
+          setValue(null)
+        },
+        variant: "plain",
+      },
+    ]
+  }
+  helperText="This is a helper text"
+  label="Label"
+  onInputValueChange={({ inputValue }) => {
+    setItems(
+      mockedOptions.filter((option) =>
+        inputValue
+          ? option.label
+              .toLocaleLowerCase()
+              .startsWith(inputValue.toLocaleLowerCase())
+          : true
+      )
+    )
+  }}
+  optionalText="optional"
+  options={items}
+  optionToString={(option) => (option ? option.label : "")}
+  placeholder="Search by organization's name or handle"
+  renderOption={(highlightedIndex, defaultStyles, option, inputValue) => (
+    <div
+      style={{
+        ...defaultStyles,
+        alignContent: "center",
+        alignItems: "center",
+        backgroundColor: defaultStyles.backgroundColor && "rgba(0,130,252,0.05)",
+        display: "flex",
+        padding: 2,
+      }}
+    >
+      <Highlight search={inputValue} text={option.label} />
+    </div>
+  )}
+  renderSelectedItem={(selectedOption) => {
+    return (
+      <span>
+        {selectedOption.label}{" "}
+        <strong>
+          <sup style={{ position: "relative", left: 12, top: -5 }}>
+            {selectedOption.A}
+          </sup>
+          <sub
+            style={{
+              position: "relative",
+              top: 5,
+            }}
+          >
+            {selectedOption.Z}
+          </sub>
+          {selectedOption.symbol}
+        </strong>
+      </span>
+    )
+  }}
+  onSelectedItemChange={({ selectedItem: selectedOption }) => {
+    setValue(selectedOption)
+  }}
+  selectedOption={value}
+  totalCount={mockedOptions.length}
+/>
+)
+```
---
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -0,0 +1,440 @@
+/** @jsxRuntime classic /*
+/** @jsx jsx */
+import React, {
+  ChangeEvent,
+  forwardRef,
+  ReactElement,
+  ReactNode,
+  Ref,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  useState,
+} from "react"
+import { HerzUITheme, jsx, SxStyleProp } from "theme-ui"
+import { useCombobox, UseComboboxStateChange } from "downshift"
+import Button, { ButtonProps } from "../Button/Button"
+
+export interface AutocompleteProps<T extends unknown> {
+  buttons?: ButtonProps[]
+  helperText?: string
+  id?: string
+  inputLabelId?: string
+  label?: string
+  onInputValueChange: (comboboxStateChange: UseComboboxStateChange<T>) => void
+  onSelectedItemChange: (changes: UseComboboxStateChange<T>) => void
+  optionalText?: string
+  options: T[]
+  optionToString: (item: T | null) => string
+  placeholder?: string
+  renderOption: (
+    highlightedIndex: number,
+    defaultStyles: SxStyleProp,
+    item: T,
+    inputValue: string,
+    index?: number,
+    array?: T[]
+  ) => ReactNode
+  renderSelectedItem?: (item: T) => ReactNode
+  required?: boolean
+  requiredText?: string
+  selectedOption?: T | null
+  status?: "error" | "loading" | "success"
+  totalCount?: number
+}
+
+export default forwardRef(function Autocomplete<T>(
+  {
+    buttons,
+    helperText,
+    id,
+    inputLabelId,
+    label,
+    onInputValueChange,
+    onSelectedItemChange,
+    optionalText,
+    options,
+    optionToString,
+    placeholder = "",
+    required,
+    requiredText,
+    renderOption,
+    renderSelectedItem,
+    selectedOption,
+    status,
+    totalCount,
+  }: AutocompleteProps<T>,
+  ref: Ref<HTMLInputElement>
+) {
+  const containerRef = useRef<HTMLDivElement>(null)
+  const inputRef = useRef<HTMLInputElement>(null)
+  useImperativeHandle<HTMLInputElement | null, HTMLInputElement | null>(
+    ref,
+    () => inputRef.current
+  )
+  const [inputValue, setInputValue] = useState("")
+  const {
+    getComboboxProps,
+    getInputProps,
+    getItemProps,
+    getLabelProps,
+    getMenuProps,
+    highlightedIndex,
+    isOpen,
+    openMenu,
+  } = useCombobox({
+    items: options,
+    itemToString: optionToString,
+    onInputValueChange,
+    onIsOpenChange: ({ isOpen: open }) => {
+      if (!open) {
+        inputRef.current?.blur()
+      } else {
+        inputRef.current?.focus()
+      }
+    },
+    onSelectedItemChange: (changes) => {
+      setInputValue(optionToString(changes.selectedItem as T | null))
+      onSelectedItemChange(changes)
+    },
+    selectedItem: selectedOption,
+  })
+
+  const styles: Record<string, SxStyleProp> = {
+    active: {
+      borderColor: "secondary.0",
+      boxShadow: (theme: HerzUITheme) =>
+        `0px 0px 0px 4px ${theme.colors.secondary.alpha[90]}`,
+      backgroundColor: "#FFF",
+    },
+  }
+
+  const state = useMemo(() => {
+    if (status) return status
+    return "default"
+  }, [status])
+
+  const inputArias = {
+    // eslint-disable-next-line unicorn/no-array-reduce
+    ...Object.keys(getInputProps()).reduce(
+      (accumulative, current) =>
+        current.includes("aria")
+          ? { ...accumulative, [current]: getInputProps()[current] }
+          : accumulative,
+      {}
+    ),
+  }
+
+  useEffect(() => {
+    if (!selectedOption) {
+      setInputValue("")
+    }
+  }, [selectedOption])
+
+  return (
+    <div
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        flexWrap: "wrap",
+        gap: 2,
+      }}
+    >
+      {label && (
+        <div {...getLabelProps()} sx={{ display: "flex", gap: 1 }}>
+          <label
+            htmlFor={id}
+            id={inputLabelId}
+            sx={{
+              color: "text.0",
+              variant: "text.body1",
+            }}
+          >
+            {label}
+          </label>
+          <span
+            sx={{
+              color: "text.40",
+              variant: "text.body2",
+            }}
+          >
+            ({required ? requiredText : optionalText})
+          </span>
+        </div>
+      )}
+      <div
+        {...getComboboxProps({
+          ref: containerRef,
+        })}
+        sx={{
+          alignItems: "center",
+          backgroundColor: "transparent",
+          border: "2px solid transparent",
+          borderRadius: 2,
+          display: "flex",
+          gap: 2,
+          outline: 0,
+          paddingX: 3,
+          paddingY: 1,
+          position: "relative",
+          transition: "all 0.2s",
+          width: "100%",
+          ...(state === "loading" ? styles.active : {}),
+          ...{
+            default: {
+              backgroundColor: selectedOption
+                ? "secondary.alpha.90"
+                : "text.alpha.95",
+            },
+            loading: {},
+            success: {
+              backgroundColor: "success.alpha.95",
+              borderColor: "success.0",
+            },
+            error: {
+              backgroundColor: "primary.alpha.95",
+              borderColor: "primary.0",
+            },
+          }[state],
+          "&:hover": {
+            ...(state === "default" && {
+              backgroundColor: selectedOption
+                ? "secondary.alpha.85"
+                : "text.alpha.90",
+            }),
+          },
+          "&:focus-within": {
+            ...(isOpen && styles.active),
+          },
+          "& *": { transition: "all 0.2s, visibility 0s" },
+        }}
+      >
+        <input
+          {...getInputProps({
+            "aria-invalid": status === "error",
+            autoComplete: "off",
+            disabled: status === "loading",
+            size: 1, // input has a default size property of 20, which limits it's minimum width. Setting it to 1 and handling width through the parent so that we can control the input width better.
+            onBlur: () => {
+              if (!selectedOption) {
+                setInputValue("")
+              } else {
+                setInputValue(optionToString(selectedOption))
+              }
+            },
+            onChange: (event: ChangeEvent<HTMLInputElement>) => {
+              setInputValue(event.target.value)
+              onInputValueChange({
+                type: useCombobox.stateChangeTypes.InputChange,
+                inputValue: event.target.value,
+              })
+            },
+            onFocus: () => {
+              if (!isOpen) openMenu()
+            },
+            placeholder,
+            ref: inputRef,
+            type: "text",
+            value: inputValue,
+          })}
+          sx={{
+            backgroundColor: "transparent",
+            border: "none",
+            color: "text.0",
+            flexGrow: 1,
+            outline: 0,
+            p: 0,
+            py: "2px", // the 2px border counts towards height, so we need 6px instead of 8px for the correct height
+            variant: "text.body1",
+            visibility: !selectedOption || isOpen ? "visible" : "hidden",
+            width: "100%",
+          }}
+        />
+        {(!selectedOption || isOpen) && (
+          <Button
+            {...inputArias}
+            aria-label="Search"
+            color="text"
+            iconName="IconSearch"
+            onClick={() => {
+              inputRef.current?.focus()
+            }}
+            size="small"
+            sx={{
+              backgroundColor: "transparent",
+              "&:hover": {
+                backgroundColor: "transparent",
+              },
+              "&:not([disabled]):hover": {
+                backgroundColor: "transparent",
+              },
+            }}
+          />
+        )}
+        {!isOpen && selectedOption && (
+          <div
+            {...inputArias}
+            onClick={() => {
+              openMenu()
+            }}
+            sx={{
+              overflow: "hidden",
+              position: "absolute",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              width: inputRef.current?.getClientRects()[0].width,
+            }}
+          >
+            {renderSelectedItem
+              ? renderSelectedItem(selectedOption)
+              : optionToString(selectedOption)}
+          </div>
+        )}
+        {buttons && buttons.length > 0 && selectedOption && !isOpen && (
+          <div
+            sx={{
+              display: "flex",
+              gap: 1,
+              whiteSpace: "nowrap",
+            }}
+          >
+            {buttons.map((button, index) => (
+              <Button
+                key={`autocompleteButton-${index}`}
+                {...button}
+                sx={{ paddingY: 0 }}
+              >
+                {button.children}
+              </Button>
+            ))}
+          </div>
+        )}
+      </div>
+      {helperText && (
+        <div
+          sx={{
+            ...{
+              default: {
+                color: "text.40",
+                variant: "text.body2",
+              },
+              loading: {},
+              success: {
+                px: 3,
+                pb: 2,
+                color: "success.0",
+                variant: "text.body1",
+              },
+              error: {
+                px: 3,
+                pb: 2,
+                color: "primary.0",
+                variant: "text.body1",
+              },
+            }[state],
+          }}
+        >
+          {helperText}
+        </div>
+      )}
+      <div
+        {...getMenuProps()}
+        sx={{
+          background: "#fff",
+          border: "1px solid #E8E8E9",
+          borderRadius: 4,
+          boxShadow: "0px 1px 12px rgba(0, 0, 0, 0.16)",
+          marginTop: 3,
+          minWidth: containerRef.current?.getClientRects()[0].width,
+          outline: "0",
+          overflowX: "hidden",
+          overflowY: "auto",
+          padding: 4,
+          paddingBottom: totalCount ? 0 : 4,
+          position: "absolute",
+          transition: "opacity .1s ease",
+          top:
+            window.scrollY +
+            (containerRef.current?.getClientRects()[0].bottom || 0),
+          visibility: isOpen ? "visible" : "hidden",
+          zIndex: 1000,
+          "& > div": {
+            cursor: "pointer",
+          },
+        }}
+      >
+        {isOpen &&
+          options.map((item, index, array) => (
+            <div {...getItemProps({ item, index })} key={index}>
+              {renderOption(
+                highlightedIndex,
+                {
+                  backgroundColor:
+                    highlightedIndex === index
+                      ? "secondary.alpha.95"
+                      : undefined,
+                  borderRadius: 2,
+                  height: 60,
+                },
+                item,
+                inputValue,
+                index,
+                array
+              )}
+            </div>
+          ))}
+        {totalCount && (
+          <div
+            sx={{
+              backgroundColor: "inherit",
+              bottom: 0,
+              padding: `16px 8px 24px`,
+              position: "sticky",
+              textAlign: "center",
+              transition: "all 0s",
+              variant: "text.body2",
+            }}
+          >
+            {options.length > 0 ? (
+              <React.Fragment>
+                Currently showing <strong>{options.length}</strong> results from
+                a total of <strong>{totalCount}</strong>.
+              </React.Fragment>
+            ) : (
+              <div
+                sx={{
+                  alignContent: "center",
+                  alignItems: "center",
+                  display: "flex",
+                  gap: 3,
+                  justifyContent: "center",
+                  transition: "all 0s",
+                }}
+              >
+                No results found{" "}
+                <Button
+                  color="secondary"
+                  onClick={() => {
+                    setInputValue("")
+                    onInputValueChange({
+                      type: useCombobox.stateChangeTypes.InputChange,
+                      inputValue: "",
+                    })
+                  }}
+                  size="small"
+                  sx={{ paddingY: 0 }}
+                  variant="plain"
+                >
+                  Clear search
+                </Button>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}) as <T extends unknown>(
+  p: AutocompleteProps<T> & { ref?: Ref<HTMLInputElement> }
+) => ReactElement
