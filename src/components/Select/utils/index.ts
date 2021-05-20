8cd861b1089e68277020657b123df11080ebd1ab
Giuseppe Battistella <giusbattistella@gmail.com>
2021-05-20 11:09:54 -0300

    fix(Select): fixed issue when children has nested arrays DEV-53

--- src/components/Select/utils/index.ts
+++ src/components/Select/utils/index.ts
@@ -2,38 +2,40 @@ import React from "react"
 import { toArray } from "../../../helpers/utils"
 import { SelectOptionType } from "../Select"
 
-export function getDataFromChildren(nodes: React.ReactNode) {
-  return (
-    toArray(nodes)
-      // eslint-disable-next-line unicorn/no-array-reduce
-      .reduce<Array<React.ReactNode>>((accumulator, node) => {
-        if (Array.isArray(node)) return [...accumulator, ...node]
-        return [...accumulator, node]
-      }, [])
-      .map((node) => {
-        if (!React.isValidElement(node) || !node.type) {
-          return null
-        }
-
-        const {
-          type: { isSelectOption, isSelectOptionCustom },
-          props: { children, value, label: propLabel },
-        } = node as React.ReactElement & {
-          type: { isSelectOption: boolean; isSelectOptionCustom: boolean }
-        }
-        if (!isSelectOption && !isSelectOptionCustom) return null
-
-        let label = children
-        if (isSelectOptionCustom) label = propLabel || "Custom..."
-
-        return {
-          label,
-          value: value,
-          isCustom: isSelectOptionCustom,
-        } as SelectOptionType
-      })
-      .filter((data) => data) as SelectOptionType[]
+function flatten<T = unknown>(array: Array<T>): Array<T> {
+  // eslint-disable-next-line unicorn/no-array-reduce
+  return array.reduce(
+    (accumulator: Array<T>, current) =>
+      accumulator.concat(Array.isArray(current) ? flatten(current) : current),
+    []
   )
+}
+
+export function getDataFromChildren(nodes: React.ReactNode) {
+  return flatten(toArray(nodes))
+    .map((node) => {
+      if (!React.isValidElement(node) || !node.type) {
+        return null
+      }
+
+      const {
+        type: { isSelectOption, isSelectOptionCustom },
+        props: { children, value, label: propLabel },
+      } = node as React.ReactElement & {
+        type: { isSelectOption: boolean; isSelectOptionCustom: boolean }
+      }
+      if (!isSelectOption && !isSelectOptionCustom) return null
+
+      let label = children
+      if (isSelectOptionCustom) label = propLabel || "Custom..."
+
+      return {
+        label,
+        value: value,
+        isCustom: isSelectOptionCustom,
+      } as SelectOptionType
+    })
+    .filter((data) => data) as SelectOptionType[]
 }
 
 export function isArrayEqual<T = number | string>(
