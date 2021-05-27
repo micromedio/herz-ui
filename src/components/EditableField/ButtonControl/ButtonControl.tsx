0f8b92da8ac4aec93a87dbbdbd13c0b3b4f1fda5
Giuseppe Battistella <giusbattistella@gmail.com>
2021-05-27 14:43:00 -0300

    feat(EditableField): Added EditableField component with Group and Text DEV-194

---
+++ src/components/EditableField/ButtonControl/ButtonControl.tsx
@@ -0,0 +1,94 @@
+/** @jsxRuntime classic /
+/** @jsx jsx */
+import React, { MouseEvent, useMemo } from "react"
+import { HerzUITheme, jsx } from "theme-ui"
+import Button from "../../Button/Button"
+import Spinner from "../../Spinner/Spinner"
+
+export interface ButtonControlProps {
+  isLoading?: boolean
+  showButtons?: boolean
+  onReset?: (event: MouseEvent<HTMLButtonElement>) => void
+  onSave?: () => void
+  children: React.ReactNode
+}
+
+const ButtonControl = ({
+  isLoading = false,
+  showButtons = false,
+  onReset,
+  onSave,
+  children,
+}: ButtonControlProps) => {
+  const buttonStyles = useMemo(
+    () => ({
+      backgroundColor: "#FFF",
+      boxShadow: (theme: HerzUITheme) =>
+        `0px 1px 12px ${theme.colors.text.alpha[85]}`,
+      borderRadius: 2,
+
+      "&:hover": {
+        backgroundColor: "text.alpha.95",
+      },
+    }),
+    []
+  )
+
+  return (
+    <div
+      sx={{
+        position: "relative",
+      }}
+    >
+      {children}
+
+      {showButtons && (
+        <div
+          sx={{
+            display: "flex",
+            position: "absolute",
+            top: "100%",
+            right: 0,
+            gap: 1,
+            pt: 2,
+            zIndex: 100,
+          }}
+        >
+          {isLoading ? (
+            <div
+              sx={{
+                display: "flex",
+                p: 1,
+                ...buttonStyles,
+                "&:hover": {},
+              }}
+            >
+              <Spinner size={16} />
+            </div>
+          ) : (
+            <React.Fragment>
+              <Button
+                size="small"
+                color="text"
+                iconName="IconX"
+                aria-label="reset"
+                onClick={onReset}
+                sx={buttonStyles}
+              />
+              <Button
+                size="small"
+                color="text"
+                iconName="IconCheck"
+                aria-label="save"
+                onClick={onSave}
+                sx={buttonStyles}
+              />
+            </React.Fragment>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ButtonControl
---
+++ src/components/EditableField/EditableField.ts
@@ -0,0 +1,11 @@
+import Group from "./Group/Group"
+import Text from "./Text/Text"
+export type { EditableFieldGroupProps } from "./Group/Group"
+export type { EditableTextProps } from "./Text/Text"
+
+const EditableField = {
+  Group,
+  Text,
+}
+
+export default EditableField
