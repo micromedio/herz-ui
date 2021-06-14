fd2f569b52816a55f54381edcc84180e8c34feb7
Weliton de Resende <welitonderesende@gmail.com>
2021-06-14 15:01:41 -0300

    fix(Popover): Tippy bug on inputs getting height: 0

---
+++ src/components/Popover/LazyTippy.tsx
@@ -0,0 +1,33 @@
+/** @jsxImportSource theme-ui */
+import { forwardRef, useState } from "react"
+import Tippy, { TippyProps } from "@tippyjs/react"
+
+const LazyTippy = forwardRef(function LazyTippy(props: TippyProps, ref) {
+  const [mounted, setMounted] = useState(false)
+
+  const lazyPlugin = {
+    fn: () => ({
+      onMount: () => setMounted(true),
+      onHidden: () => setMounted(false),
+    }),
+  }
+
+  const computedProps = { ...props }
+
+  computedProps.plugins = [lazyPlugin, ...(props.plugins || [])]
+
+  if (props.render) {
+    computedProps.render = (...arguments_) =>
+      mounted ? props.render(...arguments_) : ""
+  } else {
+    computedProps.content = mounted ? props.content : ""
+  }
+
+  return (
+    <Tippy {...computedProps} ref={ref}>
+      {props.children}
+    </Tippy>
+  )
+})
+
+export default LazyTippy
