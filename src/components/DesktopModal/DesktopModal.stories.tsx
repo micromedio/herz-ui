c327fc49670a41e1ff550730abfbb51fd7aec5c4
Michelle Horn <michellehorn@live.com>
2021-09-15 17:02:17 -0300

    fix(DesktopModal) new snapshot STC-128

--- src/components/DesktopModal/DesktopModal.stories.tsx
+++ src/components/DesktopModal/DesktopModal.stories.tsx
@@ -16,9 +16,7 @@ const Template: Story<DesktopModalProps> = (props: DesktopModalProps) => {
     <>
       <Button onClick={() => setModalOpen(!isOpen)}>Toggle Open</Button>
       <DesktopModal {...props} isVisible={isOpen}>
-        <div sx={{ display: "grid", gap: 6, p: 8, pb: 12, minWidth: 500 }}>
-          Modal info
-        </div>
+        Modal info
         <Button onClick={() => setModalOpen(false)}>Close modal</Button>
       </DesktopModal>
     </>
