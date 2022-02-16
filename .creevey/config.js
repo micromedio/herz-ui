cebdafa04626554e9a7e46de713ff6e71d0297ce
Giuseppe Battistella <giusbattistella@gmail.com>
2022-02-16 16:49:14 -0300

    feat(Breadcrumbs): added className and some default styles STC-823 (#184)
    * feat(Breadcrumbs): added className and some default styles STC-823
    
    * chore: increased creevey threshold STC-823

--- .creevey/config.js
+++ .creevey/config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  diffOptions: { threshold: 0.005 },
+  diffOptions: { threshold: 0.02 },
   maxRetries: 2,
   screenDir: path.join(__dirname, '../tests/images'),
   browsers: {
